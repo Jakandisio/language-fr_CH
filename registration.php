b251ab7d9e0e059f1b23437c84dce03739909662
Rick <rickb1998@hotmail.com>
2022-06-13 14:31:51 +0200

    changes

--- registration.php
+++ registration.php
@@ -7,4 +7,4 @@ declare(strict_types=1);
 
 use \Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::LANGUAGE, 'communityengineering_fr_fr', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::LANGUAGE, 'communityengineering_fr_ch', __DIR__);
